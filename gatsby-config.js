790ed437973c6a35dd5956c481fbb361b176e980
Jessa Pedrola <jessa@symph.co>
2020-08-04 16:58:00 +0800

    Implement Gatsby Contentful Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Contentful Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: '3l3wdx7va3xv',
+        accessToken: 'owuoX--HBJ3VKv4aPerKh7h2yEw9ScjNHaNOPoICG1A'
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve('./src/templates/blog-post.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
+              }
+            }
+          }
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        result.data.allContentfulBlogPost.edges.forEach((post) => {
+          createPage({
+            path: `/blog/${post.node.slug}/`,
+            component: blogPostTemplate,
+            context: {
+              slug: post.node.slug
+            },
+          })
+        })
+      })
+    )
+  })
+}
